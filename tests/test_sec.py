1f42acf7b174589f3f50cbafa574b76801ace4b9
ShlackBaum <95583630+ShlackBaum@users.noreply.github.com>
2022-07-21 12:45:38 +0400

    Add files via upload

--- tests/test_sec.py
+++ tests/test_sec.py
@@ -2,7 +2,8 @@ import pytest
 from unittest.mock import patch
 import unittest
 from shelf_document_tests_homework import \
-    check_document_existance, get_doc_owner_name, get_doc_shelf, remove_doc_from_shelf, delete_doc
+    check_document_existance, get_doc_owner_name, get_doc_shelf, remove_doc_from_shelf, \
+    delete_doc, add_new_shelf, append_doc_to_shelf
 
 
 FIXTURE = [("11-2", True), ("11-3", False)]
@@ -47,7 +48,21 @@ def test_remove_doc_from_shelf2():
 
 @patch('builtins.input', lambda _: '4')
 def test_add_new_shelf():
-    assert add_new_shelf() == None
+    assert add_new_shelf()[1] == True
+
+@patch('builtins.input', lambda _: '3')
+def test_add_new_shelf2():
+    assert add_new_shelf()[1] == False
+
+FIXTURE4 = [("111","1", ('1', False)), ("222","4", ('4', False)),
+            ("11-2", "1", ('1', False)), ("11-2", "6", ('6', True)),
+            ("11-2", "5", ('5', True))
+            ]
+@pytest.mark.parametrize("doc_number, shelf_number, result", FIXTURE4)
+def test_append_doc_to_shelf(doc_number, shelf_number, result):
+    assert append_doc_to_shelf(doc_number, shelf_number) == result
+
+
 
 
 
